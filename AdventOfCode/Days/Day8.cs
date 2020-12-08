817940170a0a59fd111e25fb18215ceef37ff2ef
Amber Brault <celestialamber1@gmail.com>
2020-12-08 01:21:44 -0500

    Add improved version

--- AdventOfCode/Days/Day8.cs
+++ AdventOfCode/Days/Day8.cs
@@ -18,7 +18,6 @@ namespace AdventOfCode.Days {
                 string numberString = instructionParts[1];
                 char sign = numberString[0];
                 int number = int.Parse(numberString.Substring(1)) * (sign == '+' ? 1 : -1);
-                Console.WriteLine(lines[pc] + ", /" + instruction + "/, /" + numberString + "/");
                 switch (instruction) {
                     case "acc":
                     accumulator += number;
@@ -41,6 +40,63 @@ namespace AdventOfCode.Days {
 
         }
 
+        //Improved version:
+        public static void Day8Part2() {
+            string[] lines = File.ReadAllLines("Input Files/inputday8.txt");
+            bool[] passed = new bool[lines.Length];
+            int accumulator = 0;
+            int pc = 0;
+
+            for (int i = 0; i < lines.Length; i++) {
+                accumulator = 0;
+                pc = 0;
+                passed = new bool[lines.Length];
+                string oldLine = lines[i];
+
+                if (lines[i].Contains("nop")) lines[i] = lines[i].Replace("nop", "jmp");
+                else if (lines[i].Contains("jmp")) lines[i] = lines[i].Replace("jmp", "nop");
+                else continue;
+
+                bool checkNext = false;
+
+                while (pc < lines.Length) {
+                    if (pc < lines.Length && passed[pc]) {
+                        checkNext = true;
+                        break;
+                    }
+                    passed[pc] = true;
+                    string[] instructionParts = lines[pc].Split(" ");
+                    string instruction = instructionParts[0];
+                    int number = int.Parse(instructionParts[1]);
+
+                    switch (instruction) {
+                        case "acc":
+                        accumulator += number;
+                        pc++;
+                        break;
+                        case "jmp":
+                        pc += number;
+                        break;
+                        case "nop":
+                        pc++;
+                        break;
+
+                    }
+                }
+
+                if (!checkNext) {
+                    Console.WriteLine("Changed instruction index: " + i);
+                    break;
+                }
+
+                lines[i] = oldLine;
+            }
+
+            Console.WriteLine(accumulator);
+        }
+
+        /*
+        Original version:
         public static void Day8Part2() {
             string[] lines = File.ReadAllLines("Input Files/inputday8.txt");
             bool[] passed = new bool[lines.Length];
@@ -100,8 +156,8 @@ namespace AdventOfCode.Days {
             }
 
             Console.WriteLine(accumulator);
-
         }
+        */
 
     }
 }
